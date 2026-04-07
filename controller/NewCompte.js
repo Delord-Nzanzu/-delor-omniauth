2db7fcee5962ffce71244d35ef8305811f11b581
Delord-Nzanzu <wayirenzanzu@gmail.com>
2026-04-07 13:19:00 +0100

    fix

--- controller/NewCompte.js
+++ controller/NewCompte.js
@@ -6,11 +6,18 @@ export const createPersonne = async (identite, genre, phone, email, file) => {
   formData.append("genre", genre);
   formData.append("phone", phone);
   formData.append("email", email);
-  formData.append("file", file);
+  // formData.append("file", file);
   formData.append("datecreate", new Date().toISOString());
+  if (file && file.buffer) {
+    const blob = new Blob([file.buffer], { type: file.mimetype });
+    formData.append("file", blob, file.originalname);
+  } else {
+    formData.append("file", file);
+  }
 
   const response = await fetch(`${baseUrl}/personne/save/application`, {
     method: "POST",
+    // headers: { "Content-Type": "multipart/form-data" },
     body: formData,
   });
 
@@ -20,17 +27,23 @@ export const createPersonne = async (identite, genre, phone, email, file) => {
     throw new Error(data.message || "Erreur création personne");
   }
 
-  return data?.data?.data?.idpersonne;
+  return data;
 };
 
-export const createApplication = async (idpersonne, fkorg, appId) => {
+export const createApplication = async (
+  idpersonne,
+  fkorg,
+  appId,
+  passwords,
+) => {
   const response = await fetch(`${baseUrl}/admin/save/application`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      idpersonne,
-      fkorg,
+      idpersonne: idpersonne,
+      fkorg: fkorg,
       fkapp: appId,
+      passwords: passwords,
     }),
   });
 
