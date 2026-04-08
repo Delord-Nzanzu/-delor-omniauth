9ff135fcbac65685fbcc5b4511e4ac4af4938001
Delord-Nzanzu <wayirenzanzu@gmail.com>
2026-04-08 09:03:20 +0100

    fix

---
+++ index.d.ts
@@ -0,0 +1,30 @@
+declare module "@delord/omniauth" {
+  export interface CreateCompteRouter {
+    create(
+      identite: string,
+      genre: string,
+      phone: string,
+      email: string,
+      file: any,
+    ): Promise<any>;
+
+    affecetToApp(
+      idpersonne: string,
+      fkorg: string,
+      passwords: string,
+      fkapp: string,
+    ): Promise<any>;
+  }
+
+  export interface AuthRouter {
+    login(login: string, password: string): Promise<any>;
+    refreshTokens(refreshToken: string): Promise<any>;
+  }
+
+  const omniAuth: {
+    createComppteRouter(appId: string): CreateCompteRouter;
+    authRouter(appId: string): AuthRouter;
+  };
+
+  export default omniAuth;
+}
